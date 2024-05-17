9fcb85ac004b6e41e4e44bd57597362b0bfbae37
akos-szasz-nagy <gvnsaxon+github@gmail.com>
2024-05-17 17:19:06 +0200

    Floris review

--- mb-roomgen/script.js
+++ mb-roomgen/script.js
@@ -1,53 +1,47 @@
-function newRoom() {
+const ROOM_NAMES = ['Library', 'Crypt', 'Tomb', 'Guard room'];
+const SMELLS = ['putrid smell', 'sweet scent', 'burnt smell'];
+const SOUNDS = ['eerie silence', 'knocking from the walls', 'sounds of violin', 'muffled chatter'];
+const DOORS = ['wooden door'];
+const SMELL_CLUES = ['putrid smell', 'sweet scent', 'burnt smell'];
+const SOUND_CLUES = ['eerie silence', 'knocking from the walls', 'sounds of violin', 'muffled chatter'];
+const ROOM_DRESSINGS = ['Books on the shelf - test Presence DR14 to find a sacred scroll.', 'An open grave in the middle of the room.', 'Four lit torches light the room.', 'A cluttered table with very old food on it. Very old food.'];
+const DIRECTIONS = ["north", "south", "east", "west"];
 
+function randomEntryFrom(array) {
+    return array[Math.floor(Math.random()*array.length)];
+}
+
+function createRoom() {
     // Name 
-
-    var roomNames = ['Library', 'Crypt', 'Tomb', 'Guard room'];
-    var roomName = roomNames[Math.floor(Math.random()*roomNames.length)];
-    document.getElementById("changeRoomName").innerHTML = roomName;
+    const roomName = randomEntryFrom(ROOM_NAMES);
+    document.getElementById("room-name").innerHTML = roomName;
     document.title = roomName + ' - nameless, dark places';
 
     // Adjectives 
-
-    var smells = ['putrid smell', 'sweet scent', 'burnt smell'];
-    var smell = smells[Math.floor(Math.random()*smells.length)];
-
-    var sounds = ['eerie silence', 'knocking from the walls', 'sounds of violin', 'muffled chatter'];
-    var sound = sounds[Math.floor(Math.random()*sounds.length)];
-
-    var smells2 = ['putrid smell', 'sweet scent', 'burnt smell'];
-    var smell2 = smells2[Math.floor(Math.random()*smells2.length)];
-
-    var sounds2 = ['eerie silence', 'knocking from the walls', 'sounds of violin', 'muffled chatter'];
-    var sound2 = sounds[Math.floor(Math.random()*sounds2.length)];
-
-    var doors = ['wooden door'];
-    var door = doors[Math.floor(Math.random()*doors.length)];
-
-    document.getElementById("changeAdjectives1").innerHTML = smell + ', ' + sound + '.';
-    document.getElementById("changeAdjectives2").innerHTML = door + ', ' + smell2 + ', ' + sound2 + '.';
+    const smell = randomEntryFrom(SMELLS);
+    const sound = randomEntryFrom(SOUNDS);
+    const smellClue = randomEntryFrom(SMELL_CLUES);
+    const soundClue = randomEntryFrom(SOUND_CLUES);
+    const door = randomEntryFrom(DOORS);
+    document.getElementById("room-adjectives").innerHTML = smell + ', ' + sound + '.';
+    document.getElementById("clue-adjectives").innerHTML = door + ', ' + soundClue + ', ' + smellClue + '.';
 
     // POIs 
-
-    var roomDressings = ['Books on the shelf - test Presence DR14 to find a sacred scroll.', 'An open grave in the middle of the room.', 'Four lit torches light the room.', 'A cluttered table with very old food on it. Very old food.'];
-    var roomDressing = roomDressings[Math.floor(Math.random()*roomDressings.length)];
-    document.getElementById("changeRoomDressing").innerHTML = roomDressing;
+    const roomDressing = randomEntryFrom(ROOM_DRESSINGS);
+    document.getElementById("room-dressing").innerHTML = roomDressing;
 
     // Directions
-
-    var directions = ["north", "south", "east", "west"];
-    var direction = directions[Math.floor(Math.random()*directions.length)];
-    document.getElementById("changeDirection").innerHTML = direction + ':';
-
-
-
-
-    
+    const direction = randomEntryFrom(DIRECTIONS);
+    document.getElementById("door-direction").innerHTML = direction + ':';
 }
 function moveFocus() {
-    document.getElementById("changeRoomName").focus();
+    document.getElementById("room-name").focus();
 }
 
-moveFocus();
-newRoom();
+function newRoom() {
+    createRoom();
+    moveFocus();
+}
+
+
 
